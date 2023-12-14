bcaef8486296e3591bc0d0018a0f45f56cd0d82a
lnkhe <alphonseraclon@gmail.com>
2023-12-14 04:46:35 +0100

    [4] partie 3 - production

---
+++ production/__main__.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+from utils.fonctions import *
+from utils.constantes import Tags
+
+
+##########################
+#      Variables         #
+##########################
+
+
+# Chargement des modèles d'embedding et de prédiction
+use = use_embedding_model()
+pred_model = chargement_models_predict()
+
+st.divider()
+st.title('Proposition automatique de tags pour les questions postées sur StackOverFlow')
+st.divider()
+
+titre = st.text_input('Titre de la question :')
+corpus = st.text_input('Question correspondante :')
+
+ready = st.button('Lancer la proposition de tags :point_right:')
+st.text("")
+st.text("")
+st.text("")
+
+if ready:
+
+    question = pd.Series([f'{titre} {corpus}'])
+    question_norm = step_1_3_main_normalisation_texte(question)
+
+    pred = use_and_predict(question_norm, use, pred_model)
+
+    st.subheader('Voici comment a été converti la question pour le modèle : ')
+    st.text_area('Voici comment a été converti la question pour le modèle : ', question_norm[0], label_visibility='collapsed')
+
+    st.text("")
+    st.subheader('Et voici les propositions de tags correspondants a la question : ')
+    st.write(pred)
+
+
---
+++ production/utils/fonctions.py
@@ -0,0 +1,143 @@
+# Ce script contient toutes les fonctions liées à la récupération et au prétraitement des données
+
+##########################
+#        IMPORTS         #
+##########################
+import os
+import re
+import contractions
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+import streamlit as st
+import mlflow
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+
+from utils.constantes import emoticons_regex, stop_words, Tags
+
+
+##########################
+#     CONFIGURATION      #
+##########################
+repertoire_projet = os.path.dirname(os.path.abspath(__file__))
+# Remonter les répertoires parents jusqu'à atteindre le répertoire racine du projet
+while not os.path.basename(repertoire_projet) == 'P5_classification_questions':
+    repertoire_parent = os.path.dirname(repertoire_projet)
+    if repertoire_parent == repertoire_projet:
+        raise FileNotFoundError("Répertoire racine du projet introuvable.")
+    repertoire_projet = repertoire_parent
+
+##########################
+#       Fonctions        #
+##########################
+
+@st.cache_resource
+def replace_characters(text):
+    text = re.sub(emoticons_regex, " ", text)
+    text = re.sub(r'\s+', ' ', text)
+    return text
+
+
+@st.cache_resource
+def step_1_1_clean_text(comment):
+    """
+    Cette fonction prend un commentaire en paramètre et le renvoie normalisé
+    :param comment: commentaire sous forme de chaîne de caractères
+    :return: commentaire normalisé
+    """
+    # On met tout en minuscules
+    comment = comment.lower()
+
+    # On supprime les formes contractées du texte pour uniformiser le format du texte avec la forme décontractée
+    comment = contractions.fix(comment)
+
+    # Remplacement de certaines parties du texte telles que les utilisateurs, les liens, les mentions
+    comment = re.sub(r'https?://\S+', ' ', comment)
+    comment = re.sub(r'[^\w$@#_+<>]', ' ', comment)
+    comment = re.sub(r'\s+', ' ', comment)
+
+    return comment
+
+
+@st.cache_resource
+def step_1_2_lemmatize_text(comment):
+    """
+    Cette fonction lemmatise le commente
+    :param comment: commentaire
+    :return: commentaire lemmatisé
+    """
+    # Lemmatise
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in comment.split()]
+    lemmatized_comment = ' '.join(lemmatized_words)
+
+    return lemmatized_comment
+
+
+@st.cache_resource
+def step_1_3_main_normalisation_texte(data):
+    """
+    Cette fonction prend en entrée une Serie pandas dont elle normalise le texte
+    Un DataFrame nettoyé et normalisé.
+
+    :param data: Serie pandas contenant du texte
+    :return: Serie pandas avec le texte normalisé
+    """
+
+    data = data.apply(replace_characters)\
+        .apply(step_1_1_clean_text) \
+        .apply(step_1_2_lemmatize_text) \
+        .apply(word_tokenize) \
+        .apply(lambda comm: [word for word in comm if word not in stop_words]) \
+        .apply(lambda x: ' '.join(x))
+
+    return data
+
+
+@st.cache_resource
+def use_embedding_model():
+    # Chargement du modèle Universal Sentence Encoder
+    use_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+    embed = hub.load(use_url)
+
+    # Fonction d'embedding qui prend une série pandas en entrée
+    def embed_text_series(text_series):
+
+        # Calcul de l'embedding pour chaque texte
+        embeddings = embed(text_series)
+
+        return embeddings
+
+    return embed_text_series
+
+
+@st.cache_resource
+def chargement_models_predict():
+
+    # Chargement du modèle de prediction
+    model_path = f"{repertoire_projet}/gestion_modeles/mlruns/134896038366573457/8ed5fd432c104d9ba196a3dde3499664/artifacts/Best_Model"
+    predic_model = mlflow.sklearn.load_model(model_path)
+
+    return predic_model
+
+
+
+@st.cache_resource
+def use_and_predict(data, _emb_model, _pred_model):
+    """
+
+    :param data: données à prédire sous forme de liste, Serie
+    :return: prediction des Tags
+    """
+
+    data_vec = _emb_model(data).numpy()
+    data_pred = pd.Series(_pred_model.predict(data_vec)[0], index=Tags)
+
+    pred = data_pred[data_pred == 1].index
+
+    return list(pred)
+
+
