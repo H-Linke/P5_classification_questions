ce8995eb61b4481e41ac835d7be3cfa6db3d99e1
lnkhe <alphonseraclon@gmail.com>
2023-12-14 05:36:51 +0100

    rectification pour streamlit 9

--- production/utils/fonctions.py
+++ production/utils/fonctions.py
@@ -3,6 +3,12 @@
 ##########################
 #        IMPORTS         #
 ##########################
+import nltk
+
+nltk.download('punkt')
+nltk.download("wordnet")
+nltk.download('omw-1.4')
+
 import os
 import re
 import contractions
@@ -16,19 +22,12 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
 
-# from constantes import emoticons_regex, stop_words, Tags
+from utils.constantes import emoticons_regex, stop_words, Tags
 
 st.title("JUSQUE LA CA VA")
 ##########################
 #     CONFIGURATION      #
 ##########################
-# repertoire_projet = os.path.dirname(os.path.abspath(__file__))
-# # Remonter les répertoires parents jusqu'à atteindre le répertoire racine du projet
-# while not os.path.basename(repertoire_projet) == 'P5_classification_questions':
-#     repertoire_parent = os.path.dirname(repertoire_projet)
-#     if repertoire_parent == repertoire_projet:
-#         raise FileNotFoundError("Répertoire racine du projet introuvable.")
-#     repertoire_projet = repertoire_parent
 repertoire_projet = os.getcwd().replace(r"\\", "/")
 st.write(f"REPERTOIRE : {repertoire_projet}")
 
@@ -36,39 +35,39 @@ st.write(f"REPERTOIRE : {repertoire_projet}")
 #       Constantes        #
 ##########################
 
-emoticons_regex = r"(^|\s)(:\)|[\d\s]+|I|:\(|;\)|:D|;D|:p|:P|:o|:O|:@|:s|:S|:\$|:\||:\\|:\/|:\'\\(|:\'\\)|:\*|<3)($|\s)"
-
-stop_words = {
-    'doesn', 'both', 'ourselves', 'at', "hasn't", 'you', 'will', "shouldn't", 'were', 'am', 'ours', 'won', 'me',
-    'below', 'she', 'not', 'did', 'that', 'it', 'wouldn', 'as', "you're", "should've", 'my', "isn't", "mightn't",
-    "you'd", 'be', "it's", 't', 'he', "you've", "didn't", 'him', 'so', 'under', 'any', 've', 'down', 'most', 'over',
-    "won't", "that'll", 'once', 'had', 'was', "wasn't", 'if', 'of', 'isn', "wouldn't", 'herself', 'o', 'are', 'no',
-    'just', 'theirs', 'a', 'nor', 'mustn', 'needn', 'itself', 'aren', "needn't", 'its', 'being', 'up', 'been', 'to',
-    'but', 'do', 'couldn', 'ain', 'yourself', 'or', 'than', "mustn't", 'too', 'her', 's', 're', "you'll", 'the',
-    "couldn't", 'until', 'by', "shan't", 'them', 'your', 'y', 'some', 'such', 'few', 'more', 'didn', 'out', 'through',
-    'myself', 'haven', 'himself', 'don', 'm', 'mightn', 'above', 'doing', 'shouldn', 'we', 'should', 'his', 'off',
-    "haven't", 'shan', 'because', 'whom', 'having', 'and', "she's", 'wasn', 'd', 'own', "aren't", 'then', 'has',
-    'these', "doesn't", 'themselves', 'weren', 'have', 'here', 'hasn', 'against', 'between', "weren't", 'yourselves',
-    'an', 'does', 'll', 'on', 'yours', 'from', 'our', 'there', "don't", 'hers', 'during', 'into', 'ma', "hadn't",
-    'hadn', 'those', 'this', 'i', 'is', 'for', 'in', 'with', 'can'
-}
-
-
-Tags = ['.net', '.net-core', 'ajax', 'algorithm', 'amazon-web-services', 'android', 'android-layout', 'android-studio',
-        'angular', 'angularjs', 'apache', 'apache-spark', 'api', 'arrays', 'asp.net', 'asp.net-core', 'asp.net-mvc',
-        'asp.net-web-api', 'assembly', 'async-await', 'asynchronous', 'authentication', 'bash', 'c', 'c#', 'c++',
-        'c++11', 'class', 'cocoa', 'cocoa-touch', 'css', 'database', 'dataframe', 'date', 'datetime', 'debugging',
-        'dictionary', 'django', 'docker', 'eclipse', 'entity-framework', 'exception', 'express', 'file', 'firebase',
-        'flutter', 'forms', 'function', 'gcc', 'generics', 'git', 'google-chrome', 'gradle', 'haskell', 'hibernate',
-        'html', 'http', 'image', 'ios', 'ipad', 'iphone', 'java', 'java-8', 'javascript', 'jpa', 'jquery', 'json',
-        'kotlin', 'lambda', 'language-lawyer', 'laravel', 'linq', 'linux', 'list', 'logging', 'machine-learning',
-        'macos', 'math', 'matplotlib', 'maven', 'memory', 'mongodb', 'multithreading', 'mysql', 'node.js', 'npm',
-        'numpy', 'objective-c', 'oop', 'opencv', 'optimization', 'pandas', 'performance', 'php', 'postgresql', 'python',
-        'python-2.7', 'python-3.x', 'qt', 'r', 'react-native', 'reactjs', 'regex', 'rest', 'ruby', 'ruby-on-rails',
-        'ruby-on-rails-3', 'scala', 'security', 'selenium', 'shell', 'spring', 'spring-boot', 'spring-mvc', 'sql',
-        'sql-server', 'ssl', 'string', 'swift', 't-sql', 'templates', 'testing', 'twitter-bootstrap', 'typescript',
-        'ubuntu', 'unit-testing', 'unix', 'user-interface', 'validation', 'visual-studio', 'visual-studio-2010',
-        'web-services', 'webpack', 'windows', 'winforms', 'wpf', 'x86', 'xaml', 'xcode', 'xml']
+# emoticons_regex = r"(^|\s)(:\)|[\d\s]+|I|:\(|;\)|:D|;D|:p|:P|:o|:O|:@|:s|:S|:\$|:\||:\\|:\/|:\'\\(|:\'\\)|:\*|<3)($|\s)"
+#
+# stop_words = {
+#     'doesn', 'both', 'ourselves', 'at', "hasn't", 'you', 'will', "shouldn't", 'were', 'am', 'ours', 'won', 'me',
+#     'below', 'she', 'not', 'did', 'that', 'it', 'wouldn', 'as', "you're", "should've", 'my', "isn't", "mightn't",
+#     "you'd", 'be', "it's", 't', 'he', "you've", "didn't", 'him', 'so', 'under', 'any', 've', 'down', 'most', 'over',
+#     "won't", "that'll", 'once', 'had', 'was', "wasn't", 'if', 'of', 'isn', "wouldn't", 'herself', 'o', 'are', 'no',
+#     'just', 'theirs', 'a', 'nor', 'mustn', 'needn', 'itself', 'aren', "needn't", 'its', 'being', 'up', 'been', 'to',
+#     'but', 'do', 'couldn', 'ain', 'yourself', 'or', 'than', "mustn't", 'too', 'her', 's', 're', "you'll", 'the',
+#     "couldn't", 'until', 'by', "shan't", 'them', 'your', 'y', 'some', 'such', 'few', 'more', 'didn', 'out', 'through',
+#     'myself', 'haven', 'himself', 'don', 'm', 'mightn', 'above', 'doing', 'shouldn', 'we', 'should', 'his', 'off',
+#     "haven't", 'shan', 'because', 'whom', 'having', 'and', "she's", 'wasn', 'd', 'own', "aren't", 'then', 'has',
+#     'these', "doesn't", 'themselves', 'weren', 'have', 'here', 'hasn', 'against', 'between', "weren't", 'yourselves',
+#     'an', 'does', 'll', 'on', 'yours', 'from', 'our', 'there', "don't", 'hers', 'during', 'into', 'ma', "hadn't",
+#     'hadn', 'those', 'this', 'i', 'is', 'for', 'in', 'with', 'can'
+# }
+#
+#
+# Tags = ['.net', '.net-core', 'ajax', 'algorithm', 'amazon-web-services', 'android', 'android-layout', 'android-studio',
+#         'angular', 'angularjs', 'apache', 'apache-spark', 'api', 'arrays', 'asp.net', 'asp.net-core', 'asp.net-mvc',
+#         'asp.net-web-api', 'assembly', 'async-await', 'asynchronous', 'authentication', 'bash', 'c', 'c#', 'c++',
+#         'c++11', 'class', 'cocoa', 'cocoa-touch', 'css', 'database', 'dataframe', 'date', 'datetime', 'debugging',
+#         'dictionary', 'django', 'docker', 'eclipse', 'entity-framework', 'exception', 'express', 'file', 'firebase',
+#         'flutter', 'forms', 'function', 'gcc', 'generics', 'git', 'google-chrome', 'gradle', 'haskell', 'hibernate',
+#         'html', 'http', 'image', 'ios', 'ipad', 'iphone', 'java', 'java-8', 'javascript', 'jpa', 'jquery', 'json',
+#         'kotlin', 'lambda', 'language-lawyer', 'laravel', 'linq', 'linux', 'list', 'logging', 'machine-learning',
+#         'macos', 'math', 'matplotlib', 'maven', 'memory', 'mongodb', 'multithreading', 'mysql', 'node.js', 'npm',
+#         'numpy', 'objective-c', 'oop', 'opencv', 'optimization', 'pandas', 'performance', 'php', 'postgresql', 'python',
+#         'python-2.7', 'python-3.x', 'qt', 'r', 'react-native', 'reactjs', 'regex', 'rest', 'ruby', 'ruby-on-rails',
+#         'ruby-on-rails-3', 'scala', 'security', 'selenium', 'shell', 'spring', 'spring-boot', 'spring-mvc', 'sql',
+#         'sql-server', 'ssl', 'string', 'swift', 't-sql', 'templates', 'testing', 'twitter-bootstrap', 'typescript',
+#         'ubuntu', 'unit-testing', 'unix', 'user-interface', 'validation', 'visual-studio', 'visual-studio-2010',
+#         'web-services', 'webpack', 'windows', 'winforms', 'wpf', 'x86', 'xaml', 'xcode', 'xml']
 
 
 ##########################
